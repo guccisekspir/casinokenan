510dec4a0be9d5d52a054750b5f0780d44f5df95
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-11-06 21:57:22 +0300

    gerekli sistemsel değişilikler yapıldı( android status bar gizleme debug silme)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:casinokenan/helpers/notificationHelper.dart';
 import 'package:casinokenan/pages/landPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
