05ba9408e2a5a6db7219d7cb36a5c584a3254cde
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-11-06 19:52:27 +0300

    navBar pages added

---
+++ lib/pages/contentPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class ContentPage extends StatefulWidget {
+  @override
+  _ContentPageState createState() => _ContentPageState();
+}
+
+class _ContentPageState extends State<ContentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red,);
+  }
+}
---
+++ lib/pages/tacticsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class TacticsPage extends StatefulWidget {
+  @override
+  _TacticsPageState createState() => _TacticsPageState();
+}
+
+class _TacticsPageState extends State<TacticsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.purpleAccent,);
+  }
+}
