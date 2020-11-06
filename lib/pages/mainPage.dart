1650f531c3a187b4d0940c3838d4f1930c17b121
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-11-06 22:41:43 +0300

    logolar eklendi
    click button yazıldı

--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -1,4 +1,7 @@
+import 'package:casinokenan/widgets/clickyButton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_glow/flutter_glow.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -6,15 +9,65 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  double height;
+  double width;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context).size.width,
           color: Colors.black,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                children: [],
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child:ClickyButton(
+                  child: Text("Twitch"),
+                  onPressed: (){
+
+                  },
+                  color: Colors.purple,
+
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child:ClickyButton(
+                  child: Text("Youtube"),
+                  onPressed: (){
+
+                  },
+                  color: Colors.red,
+
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child:ClickyButton(
+                  child: Text("Discord"),
+                  onPressed: (){
+
+                  },
+                  color: Colors.blueGrey,
+
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child:ClickyButton(
+                  child: Text("Twitter"),
+                  onPressed: (){
+
+                  },
+                  color: Colors.cyan,
+
+                ),
               )
             ],
           )),
