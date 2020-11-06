c4c64b81dda4418df4832d254497e9ea48e49c8a
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2020-11-06 19:50:47 +0300

    navigation bar yapısı yazıldı

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,7 @@
+import 'package:casinokenan/blocs/navBarBloc.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +9,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  BottomNavBarBloc _bottomNavBarBloc;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _bottomNavBarBloc = BottomNavBarBloc();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _bottomNavBarBloc.close();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.orange,);
+    GlobalKey _curvedKey = GlobalKey();
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: StreamBuilder(
+        //bloc yapımızı seçimlerden haberdar etmek
+        stream: _bottomNavBarBloc.itemStream,
+        initialData: _bottomNavBarBloc.defaultItem,
+        builder:
+        // ignore: missing_return
+            (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
+          switch (snapshot.data) {
+            case NavBarItem.PROFILE:
+
+            case NavBarItem.SHOP:
+            case NavBarItem.TROPHY:
+            case NavBarItem.TICK:
+          }
+        },
+      ),
+      bottomNavigationBar: SafeArea(
+        //BottomNavBar'ı telefon ui'ı engellememesi için
+        child: StreamBuilder(
+          //blocdaki streami dinlemek
+            stream: _bottomNavBarBloc.itemStream,
+            initialData: _bottomNavBarBloc.defaultItem,
+            builder: (context, snapshot) {
+              return CurvedNavigationBar(
+                key: _curvedKey,
+                index: 0,
+                height: 50.0,
+                items: <Widget>[
+                  Icon(LineAwesomeIcons.home,
+                      color: Theme.of(context).primaryColor, size: 30),
+                  Icon(LineAwesomeIcons.code,
+                      color: Theme.of(context).primaryColor, size: 30),
+                  Icon(LineAwesomeIcons.user,
+                      color: Theme.of(context).primaryColor, size: 30),
+                  Icon(LineAwesomeIcons.user,
+                      color: Theme.of(context).primaryColor, size: 30),
+                ],
+                buttonBackgroundColor: Theme.of(context).accentColor,
+                color: Theme.of(context).accentColor,
+                backgroundColor: Theme.of(context).primaryColor,
+                onTap: (index) {
+                  _bottomNavBarBloc.pickItem(index);
+                },
+              );
+            }),
+      ),
+    );
   }
 }
